fa62b1c5a2a495c4db74bd8d82e6cc42e47f888f
Maria Gintuft <marvalvlad@gmail.com>
2021-12-07 21:27:04 +0300

    eslint

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-import "../css/style.css"
+import '../css/style.css'
 import { Car } from './car'
 import { Truck } from './truck'
 import { SportCar } from './sport-car'
@@ -6,11 +6,10 @@ import { SportCar } from './sport-car'
 const car = new Car('Toyota', '2015', '5', '300')
 car.getinfo()
 
-const sportCar = new SportCar ('Lancia', '2009', '6.7', '500', '150','150')
+const sportCar = new SportCar('Lancia', '2009', '6.7', '500', '150', '150')
 sportCar.start()
 sportCar.getinfo()
 
-const truck = new Truck('BMW', '2010', '15', '450','200', '25')
+const truck = new Truck('BMW', '2010', '15', '450', '200', '25')
 
 truck.getinfo()
-
--- src/js/car.js
+++ src/js/car.js
@@ -11,21 +11,21 @@
 //   - метод calcFuel(distance) возвращает объем топлива на указанную distance
 
 class Car {
-    constructor(brand, year, fuelRate, distance) {
-        this.brand = brand
-        this.year = year
-        this.fuelRate = fuelRate
-        this.distance = distance
-        
-    }
-    getinfo() {
-        console.log('Бренд: '+ this.brand + ', год выпуска: ' + this.year + ', расход топлива: ' + this.fuelRate + ' л/100км')
-        console.log(`Расход топлива на ${this.distance} км = ${this.calcFuel()} литров`)
-    }
-    calcFuel() {
-        const fuelConsumption = this.distance /100 * this.fuelRate
-        return fuelConsumption
-    }
+  constructor (brand, year, fuelRate, distance) {
+    this.brand = brand
+    this.year = year
+    this.fuelRate = fuelRate
+    this.distance = distance
+  }
 
+  getinfo () {
+    console.log('Бренд: ' + this.brand + ', год выпуска: ' + this.year + ', расход топлива: ' + this.fuelRate + ' л/100км')
+    console.log(`Расход топлива на ${this.distance} км = ${this.calcFuel()} литров`)
+  }
+
+  calcFuel () {
+    const fuelConsumption = this.distance / 100 * this.fuelRate
+    return fuelConsumption
+  }
 }
 export { Car }
